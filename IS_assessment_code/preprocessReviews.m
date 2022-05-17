0c8cc79e89cc47ee33d03147ebf8b1246ad9d147
Jack-NUni <78220664+Jack-NUni@users.noreply.github.com>
2022-05-17 21:40:17 +0100

    delete
    delete

--- IS_assessment_code/preprocessReviews.m
+++
@@ -1,9 +0,0 @@
-function [documents] = preprocessReviews(textData)
-% Convert all of the text data into lowercase.
-cleanTextData = lower(textData);
-% Tokenize the textData.
-documents = tokenizedDocument(cleanTextData);
-% Erase all of the punctuation. 
-documents = erasePunctuation(documents);
-% Remove a list of stop words.
-documents = removeStopWords(documents); end
